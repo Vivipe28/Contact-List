419cfe93daa55d7e4fad34bed5edf95d46914481
Vivipe28 <viviana.perez28@gmail.com>
2022-07-14 01:55:19 -0500

    crud done

--- CL/src/app/app.component.ts
+++ CL/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CL';
+
+  selectedContact:Contact = new Contact();
+
+  contactArray:Contact[] = [
+    {
+      id: 1,
+      name:'Pedro',
+      phone:'3138621560',
+      birthdate: '1988-09-02',
+      address:'Calle Luna',
+      mail: 'moon@gmail.com'
+  },
+  {
+    id: 2,
+    name:'Laura',
+    phone:'3132287717',
+    birthdate: '1996-03-15',
+    address:'Calle Sol',
+    mail: 'sun@gmail.com'
+}
+  ];
+
+  saveOrEdit(){
+    if(this.selectedContact.id === 0){
+      this.selectedContact.id = this.contactArray.length + 1;
+      this.contactArray.push(this.selectedContact);
+    }
+    
+    this.selectedContact = new Contact();
+  }
+
+  clickEdit(contact:Contact){
+    this.selectedContact = contact;
+  }
+
+  delete(){
+    if(confirm('Estas seguro(a) de eliminar tu contacto?')){
+      this.contactArray = this.contactArray.filter(contact => contact != this.selectedContact);
+      this.selectedContact = new Contact;
+    }
+  }
 }
---
+++ CL/src/app/models/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+    id:number = 0;
+    name!:any;
+    phone!:any;
+    birthdate:any;
+    address!:any;
+    mail!: any
+}
